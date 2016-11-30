2e52e5251bbad1de6dd38b22de73d4a315745d05
tranhuynduc <tranhuynhduc.kt@gmail.com>
2016-11-30 15:18:21 +0700

    Project admin page

---
+++ admin_page/main.js
@@ -0,0 +1,34 @@
+/**
+ * Created by duc.tranhuynh on 11/30/2016.
+ */
+$(document).ready(function () {
+
+    var trigger = $('.hamburger'),
+        overlay = $('.overlay'),
+        isClosed = false;
+
+    trigger.click(function () {
+        hamburger_cross();
+    });
+
+    function hamburger_cross() {
+
+        if (isClosed == true) {
+            overlay.hide();
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            isClosed = false;
+        } else {
+            overlay.show();
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            isClosed = true;
+        }
+    }
+
+    $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+    });
+
+
+});
